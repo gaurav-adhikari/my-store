5faa0c9b39836c5767506d7503dc28cf9c9d1c5b
Gaurav <gaurav@cool-inspiron.domain.name>
2022-05-28 17:58:06 +0545

    feat(component-header) changed header name

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ function Header() {
     return (
         <nav className='bg-gray-200 border-blue-50 px-2.5 py-4 rounded shadow-xl'>
             <div className='container px-6 ml-3 font-bold text-lg'>
-                <span>Fake Store</span>
+                <span>My Store</span>
             </div>
         </nav>
     )
